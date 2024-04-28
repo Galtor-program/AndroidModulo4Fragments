0e3ed6fbfd8b51da7c9cab98c4881f85d2e8c4c5
galtor-program <felipetorog@gmail.com>
2024-04-28 19:06:48 -0400

    first commit

---
+++ app/src/main/java/com/example/evaluacionintegradoramodulo4/fragments/Login.java
@@ -0,0 +1,37 @@
+package com.example.evaluacionintegradoramodulo4.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.evaluacionintegradoramodulo4.R;
+
+
+public class Login extends Fragment {
+
+
+    public Login() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/evaluacionintegradoramodulo4/fragments/LoginPage.java
@@ -0,0 +1,36 @@
+package com.example.evaluacionintegradoramodulo4.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.evaluacionintegradoramodulo4.R;
+
+
+public class LoginPage extends Fragment {
+
+
+    public LoginPage() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login_page, container, false);
+    }
+}
